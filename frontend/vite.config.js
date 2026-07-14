bb79bec9b26838a7d9bdc9267fef3801fb47b1e9
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2026-07-14 10:21:39 +0530

    CHanges

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  server: { port: 5173 },
+  build: {
+    outDir: "dist",
+  },
+  server: { 
+    port: 5173 
+  },
 });
