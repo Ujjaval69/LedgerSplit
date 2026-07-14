61629385757ba51c113bb27ab3cc6bc98649c36c
Ujjaval Goyal <ujjavalgoyal5@gmail.com>
2026-07-14 10:15:51 +0530

    Update backend URL to Render

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: "https://ledgersplit.onrender.com/api",   // ← ye change kar
 });
 
 api.interceptors.request.use((config) => {
@@ -10,4 +10,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default api;c
